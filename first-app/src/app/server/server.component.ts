59be9d65c8b78f942714f7b3cd66663b4bebf720
Vince Saykao <90466599+VinceSaykao@users.noreply.github.com>
2022-06-15 15:09:10 -0500

    added component decorater and selector, made new html file server

---
+++ first-app/src/app/server/server.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+// decorater enhance classes and elements to 
+// Component decorator
+// it knows this is typescript
+// then pass javascript object to configure, without configuration its not good for angular 
+// html tag selector should be a string 
+@Component ({
+
+    selector: 'app-server', // can use this selector in other components
+    templateUrl: './server.component.html',
+
+})
+
+export class ServerComponent {
+
+}

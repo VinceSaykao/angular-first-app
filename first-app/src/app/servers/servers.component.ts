b405d7e7b664f7180be8b2fe5516d89692c95bb7
Vince Saykao <90466599+VinceSaykao@users.noreply.github.com>
2022-06-15 18:08:38 -0500

    added property boolean

--- first-app/src/app/servers/servers.component.ts
+++ first-app/src/app/servers/servers.component.ts
@@ -3,15 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   // made into an attribute
   selector: '[app-servers]',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>
-  `,
-  // templateUrl: './servers.component.html',
+
+  //In-line template
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>
+  // `,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 
 })
 export class ServersComponent implements OnInit {
+
+  // property holding a boolean value
+  allowNewServer = false;
 
   constructor() { }
 
